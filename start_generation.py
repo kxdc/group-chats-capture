0eb30eaafd85fa81961eb446ea2a193e29ca8aa7
kxdc <kvuexlu@gmail.com>
2021-04-28 05:28:48 +0800

    Formate codes in 'start_generation.py'
    Based on the output of following command:
    
        yapf -d start_generation.py

--- start_generation.py
+++ start_generation.py
@@ -4,7 +4,9 @@ from datetime import date
 from itchat.content import *
 from pathlib import Path
 
-@itchat.msg_register([TEXT, PICTURE, RECORDING, ATTACHMENT, VIDEO], isGroupChat=True)
+
+@itchat.msg_register([TEXT, PICTURE, RECORDING, ATTACHMENT, VIDEO],
+                     isGroupChat=True)
 def information(msg):
 
     # collect following field of received group char message
@@ -16,12 +18,12 @@ def information(msg):
     msg_type = msg['Type']
 
     single_msg = '[{gname}] [{nname}] [{rtime}] [{mtype}] "{mcontent}" [{fname}]\n'.format(
-                         gname = msg_group_name,
-                         nname = msg_nickname,
-                         rtime = ctime(msg_create_time),
-                         mtype = msg_type,
-                         mcontent = msg_content,
-                         fname = msg_from_uname)
+        gname=msg_group_name,
+        nname=msg_nickname,
+        rtime=ctime(msg_create_time),
+        mtype=msg_type,
+        mcontent=msg_content,
+        fname=msg_from_uname)
     print(single_msg)
 
     # save the txt file with the name of group name
@@ -38,24 +40,22 @@ def after_login():
     chat_rooms = itchat.search_chatrooms(name=target_group_name)
 
     if len(chat_rooms) > 0:
-        print('[{stime}]: found group chat "{gname}"'
-            .format(
-                stime = ctime(time()),
-                gname = target_group_name))
+        print('[{stime}]: found group chat "{gname}"'.format(
+            stime=ctime(time()), gname=target_group_name))
     else:
-        print('[{stime}]: not found group chat "{gname}" !!!'
-            .format(
-                stime = ctime(time()),
-                gname = target_group_name))
+        print('[{stime}]: not found group chat "{gname}" !!!'.format(
+            stime=ctime(time()), gname=target_group_name))
         exit(1)
+
 
 def after_exit():
     print("exit !!")
 
+
 if __name__ == '__main__':
 
     filename = ''
-    target_group_name = '' # check whether the group is found
+    target_group_name = ''  # check whether the group is found
     itchat.auto_login(enableCmdQR=2,
                       hotReload=True,
                       loginCallback=after_login,
